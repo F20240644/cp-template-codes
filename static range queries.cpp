b17c42e38daac7e704eb70dcd7c3dfb26f2a23a7
F20240644 <ayushkumarsingh.github@gmail.com>
2025-09-07 14:33:08 +0530

    Changed classes to structs

--- static range queries.cpp
+++ static range queries.cpp
@@ -3,12 +3,9 @@ using namespace std;
 
 //1-D prefix sums
 
-class PrefixSum
+struct PrefixSum
 {
-private:
     std::vector<long long> ps;
-
-public:
     PrefixSum(const std::vector<int> &v)
     {
         int n = v.size();
@@ -24,6 +21,36 @@ public:
     }
 };
 
+
+//sparse table (for minm)
+
+struct SparseTable
+{
+    int n, LOG;
+    std::vector<std::vector<int>> table;
+    SparseTable(const std::vector<int> &v)
+    {
+        n = v.size();
+        LOG = 32 - __builtin_clz(n);
+        table.assign(n, std::vector<int>(LOG));
+        for (int i = 0; i < n; i++)
+        {
+            table[i][0] = v[i];
+        }
+        for (int j = 1; j < LOG; j++)
+        {
+            for (int i = 0; i + (1 << j) <= n; i++)
+            {
+                table[i][j] = std::min(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+    int query(int L, int R)
+    {
+        int j = 31 - __builtin_clz(R - L + 1);
+        return std::min(table[L][j], table[R - (1 << j) + 1][j]);
+    }
+};
 
 int main(){
 
